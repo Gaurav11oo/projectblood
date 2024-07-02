416740c27e93738f113d5cf593070fcc55531be8
Gaurav_nishad <111732376+Gaurav11oo@users.noreply.github.com>
2024-07-02 15:55:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
-const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
@@ -16,27 +15,6 @@ dotenv.config();
 //mongodb connection
 connectDB();
 
-//mongodb
-const mongoURI = "mongodb+srv://project12024:project2024@cluster1.qnt3tf5.mongodb.net/";
-
-try {
-  mongoose
-    .connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("Connected to MongoDB");
-      app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    })
-    .catch((err) => {
-      console.error("Could not connect to MongoDB:", err)
-      process.exit(1)
-    });
-
-} catch (err) {
-  console.log("Error : " + err.message)
-}
 
 
 //rest object
@@ -65,7 +43,7 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 //listen
 app.listen(PORT, () => {
   console.log(
