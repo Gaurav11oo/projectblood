d7e4d4db5a59bbebcd14440cb1ac404568431632
Gaurav_nishad <111732376+Gaurav11oo@users.noreply.github.com>
2024-07-02 14:55:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
